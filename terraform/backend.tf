a0d79d35ac737b142feaa34ecb4097c31cdbc957
jpbarto <jason.p.barto@gmail.com>
2025-10-14 15:50:34 +0100

    added terraform backend config for this environment

---
+++ terraform/backend.tf
@@ -0,0 +1,16 @@
+variable "cicd_environment_name" {
+  type = "string"
+}
+
+variable "cicd_application_name" {
+  type = "string"
+  description = "Name of the application. The name will be used in an S3 object key and must conform to S3 naming convention."
+}
+
+terraform {
+  backend "s3" {
+    bucket = "cicd-environ-tofu-state-prod-ab12cf"
+    key = "${var.cicd_environment_name}/${var.cicd_application_name}/terraform.tfstate"
+    region = "us-east-2"
+  }
+}
